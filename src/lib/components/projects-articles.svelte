6f8b4d4da56e1ba991d046e137ab06606c5a2be1
KyanTG <kyan.gijtenbeek@hva.nl>
2026-03-04 11:53:15 +0100

    feature: each loop for project articles

---
+++ src/lib/components/projects-articles.svelte
@@ -0,0 +1,12 @@
+<script>
+    import projects from '$lib/data/projects.json';
+</script>
+
+<section class="project-articles">
+    {#each projects as project}
+        <article class="project-article">
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+        </article>
+    {/each}
+</section>
