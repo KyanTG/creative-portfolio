29ec9842e950f49bc954088cec60f5002be68a3d
KyanTG <kyan.gijtenbeek@hva.nl>
2026-04-15 14:27:09 +0200

    working on clamp with the images and changed the images

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -11,7 +11,7 @@
             <h1>{project.title}</h1>
             <p class="description">{project.description}</p>
 
-            <section>
+            <section class="project-images">
                 <img class="website-phone" src={project.websitePhone} alt={project.title}>
                 <img class="website-tablet" src={project.websiteTablet} alt={project.title}>
                 <img class="website-desktop" src={project.websiteDesktop} alt={project.title}>
@@ -31,19 +31,55 @@
 <style>
     main {
         padding-inline: 2rem;
+        width: 100%;
+
+        @media ( min-width: 700px ) {
+            padding-inline: 3rem;
+        }
+
+        @media ( min-width: 1024px ) {
+            padding-inline: 4rem;
+        }
     }
 
-    h1 {
-        padding-top: 6rem;
-    }
-
-    article {
+    .project-container {
         display: flex;
         flex-direction: column;
     }
 
+    h1 {
+        max-width: 250px;
+        padding-top: 2.5rem;
+
+        @media ( min-width: 400px ) {
+            max-width: fit-content;
+        }
+    }
+
     p {
-        font-size: 16px;
+        padding-top: 2rem;
+        max-width: 700px;
+    }
+
+    .project-images {
+        display: flex;
+        flex-direction: column;
+        padding-top: 8rem;
+        width: 100%;
+    }
+
+    img {
+        object-fit: contain;
+        width: auto;
+        height: clamp(160px, calc(160px + (100vw - 320px) * 0.5), 220px);
+
+    }
+
+    p {
         color: var(--secondary-color);
     }
+
+    /* make stacking cards animation for mobile */
+
+    /* make animation for the projects on tablet and desktop, also design that */
 </style>
